0932d3da839f912d22a4e1f4af11406800c848b5
Nagagitacc <nagaraj07011997@gmail.com>
2023-02-17 18:28:36 +0530

    Final Commit

--- src/App.js
+++ src/App.js
@@ -1,34 +1,73 @@
 import hotBg from "./asserts/hot.jpg";
 import coldBg from "./asserts/cold.jpg";
 import Description from "./components/Description";
+import { useEffect, useState } from "react";
+import { getFormatterWeatherData } from "./components/weatherService";
 
 function App() {
+  const [city, setCity] = useState("Chennai");
+  const [weather, setWeather] = useState(null);
+  const [units, setUnits] = useState("metric");
+  const [bg, setBg] = useState(hotBg);
+
+  useEffect(() => {
+    const fetchWeatherData = async () => {
+      const data = await getFormatterWeatherData(city, units);
+      setWeather(data);
+      const threshold = units === "metric" ? 20 : 68;
+      if (data.temp <= threshold) setBg(coldBg);
+      else setBg(hotBg);
+    };
+    fetchWeatherData();
+  }, [units, city]);
+
+  const handleUnitsClick = (e) => {
+    const button = e.currentTarget;
+    const currentUnit = button.innerText.slice(1);
+    console.log("button.innerText", button.innerText);
+
+    const isCelsius = currentUnit === "C";
+    button.innerText = isCelsius ? "°F" : "°C";
+    setUnits(isCelsius ? "metric" : "imperial");
+  };
+
+  const enterKeyPressed = (e) => {
+    if (e.keyCode === 13) {
+      setCity(e.currentTarget.value);
+      e.currentTarget.blur();
+    }
+  };
+
   return (
-    <div className="app" style={{ backgroundImage: `url(${coldBg})` }}>
+    <div className="app" style={{ backgroundImage: `url(${bg})` }}>
       <div className="overlay">
-        <div className="container">
-          <div className="section section__inputs">
-            <input type="text" name="city" placeholder="Enter City..." />
-            <button>°F</button>
-          </div>
-          <div className="section section__temperature">
-            <div className="icon">
-              <h3>London, GB</h3>
-              <img
-                src="https://openweathermap.org/img/wn/02d@2x.png"
-                alt="weatherIcon"
+        {weather && (
+          <div className="container">
+            <div className="section section__inputs">
+              <input
+                onKeyDown={enterKeyPressed}
+                type="text"
+                name="city"
+                placeholder="Enter City..."
               />
-              <h3>Cloudy</h3>
+              <button onClick={(e) => handleUnitsClick(e)}>°F</button>
             </div>
-            <div className="temperature">
-              <h1>34 °C</h1>
+            <div className="section section__temperature">
+              <div className="icon">
+                <h3>{`${weather.name}, ${weather.country}`}</h3>
+                <img src={weather.iconURL} alt="weatherIcon" />
+                <h3>{weather.description}</h3>
+              </div>
+              <div className="temperature">
+                <h1>{`${weather.temp.toFixed()} °${
+                  units === "metric" ? "C" : "F"
+                }`}</h1>
+              </div>
             </div>
+            {/* Bottom Description */}
+            <Description weather={weather} units={units} />
           </div>
-          {/* Bottom Description */}
-          <Description />
-          <small>min</small>
-          <h2>32 °C</h2>
-        </div>
+        )}
       </div>
     </div>
   );
